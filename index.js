ebaa295cf453e368e21b7e29da1486fc957b8501
PipoMain <daniel.acosta.da@gmail.com>
2022-09-22 16:33:37 -0300

    nuevas funciones

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 class manga  {
-    constructor(Titulo,Imagen,descripcion,){
+    constructor(id,Titulo,Imagen,descripcion,precio,){
+        this.id = id;
         this.Titulo = Titulo;
         this.Imagen = Imagen;
         this.descripcion = descripcion;
+        this.precio = precio
     }
 }
 
-const manga1 = new manga("Bleach","./imagenes/bleach.webp","La serie narra las aventuras de un adolescente llamado Ichigo Kurosaki, un estudiante de quince años que tiene la habilidad de interactuar con los espíritus.");
+const manga1 = new manga("1","Bleach","./imagenes/bleach.webp","La serie narra las aventuras de un adolescente llamado Ichigo Kurosaki, un estudiante de quince años que tiene la habilidad de interactuar con los espíritus.","1350$");
 
-const manga2 = new manga("Demon Slayer","./imagenes/demon slayer.webp","Demon Slayer se ubica en un mundo donde los demonios y sus cazadores tienen ciertos roles y actúan de acuerdo a reglas ya establecidas. Por fortuna nos adentramos en este mundo al mismo tiempo que el protagonista, así que junto a él vamos descubriendo la complejidad del mundo en esta historia.");
+const manga2 = new manga("2","Demon Slayer","./imagenes/demon slayer.webp","Demon Slayer se ubica en un mundo donde los demonios y sus cazadores tienen ciertos roles y actúan de acuerdo a reglas ya establecidas. Por fortuna nos adentramos en este mundo al mismo tiempo que el protagonista, así que junto a él vamos descubriendo la complejidad del mundo en esta historia.","750$");
 
-const manga3 = new manga("Evangelion","./imagenes/evangelionmanga.webp","Evangelion es una serie “de robots” donde los robots enemigos son lo menos importante pero que a la vez hacen avanzar la trama.");
+const manga3 = new manga("3","Evangelion","./imagenes/evangelionmanga.webp","Evangelion es una serie “de robots” donde los robots enemigos son lo menos importante pero que a la vez hacen avanzar la trama.","900$");
 
 const mangas = []
 
@@ -27,7 +29,10 @@ function mostrarMangas(mangas) {
        divmangacontainer.innerHTML = `
        <img src="${manga.Imagen}" alt="${manga.Titulo}">
        <h2>${manga.Titulo}</h2>
+       <h3>${manga.id}</h3>
        <p>Descripcion: ${manga.descripcion}</p>
+       <br>
+       <p>Precio: ${manga.precio}</p>
        `;
        mangaContainer.append(divmangacontainer);
        const botonCompra = crearBotonComprar(manga)
@@ -37,6 +42,28 @@ function mostrarMangas(mangas) {
 } 
 
 
+
+function comprarManga(e) {
+    
+    const id = document.getElementById("id").value;
+    const Titulo = document.getElementById("Titulo").value;
+    const Imagen = document.getElementById("Imagen").value;
+    const descripcion = document.getElementById("descripcion").value;
+    const precio = document.getElementById("precio").value;
+
+    const manga = new manga(id,Titulo,Imagen,descripcion,precio);
+    const mangasEnLocaLStorage = JSON.parse(localStorage.getItem(mangas));
+
+ if (mangasEnLocaLStorage == null) {
+    localStorage.setItem(mangas, JSON.stringify([manga]));
+    mostrarMangas([manga]);
+ }else {
+    mangasEnLocaLStorage.push(manga);
+    localStorage.setItem(mangas, JSON.stringify(mangasEnLocaLStorage));
+    mostrarMangas(mangasEnLocaLStorage);
+ }
+ e.target.reset();
+}
 function crearBotonComprar(manga) {
     const buttom = document.createElement("button");
     buttom.innerText = "Comprar";
